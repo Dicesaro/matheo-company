f718b67ed5875210bd675afe6d2b845b7709075e
Cesar <cesarluis4056@gmail.com>
2026-04-04 16:57:03 -0500

    fix:pagina contacto

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,15 +1,14 @@
-"use client";
-import { useEffect } from 'react';
-import Contact from '../components/Contact';
+import { Suspense } from 'react'
+import Contact from '../components/Contact'
+import ScrollToTop from '../components/ScrollToTop'
 
 export default function ContactPage() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
-    <div className="pt-24 md:pt-32">
-      <Contact />
-    </div>
-  );
+    <Suspense fallback={<div>Cargando...</div>}>
+      <ScrollToTop />
+      <div className="pt-24 md:pt-32">
+        <Contact />
+      </div>
+    </Suspense>
+  )
 }
